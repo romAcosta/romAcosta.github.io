0aa4e3f984669f1298b5dba7202e142b545acb91
romAcosta <romeroacosta992@gmail.com>
2024-11-13 17:21:59 -0700

    Initial Commit

---
+++ ScattergoriesTogetherGUI/src/main/java/com/example/ScattergoriesTogetherGUI/ScattergoriesTogetherGuiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ScattergoriesTogetherGUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScattergoriesTogetherGuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScattergoriesTogetherGuiApplication.class, args);
+	}
+
+}
---
+++ ScattergoriesTogetherGUI/src/test/java/com/example/ScattergoriesTogetherGUI/ScattergoriesTogetherGuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ScattergoriesTogetherGUI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScattergoriesTogetherGuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
