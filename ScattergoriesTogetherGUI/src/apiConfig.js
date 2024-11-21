11bdcc0bb356d01b6663f66beb69f95d721bbd03
merlyngilchrist <157403080+merlyngilchrist@users.noreply.github.com>
2024-11-21 15:52:42 -0700

    api connected

---
+++ ScattergoriesTogetherGUI/src/apiConfig.js
@@ -0,0 +1,19 @@
+const API_BASE_URL = "https://scattergoriestogetherapi.onrender.com";
+
+export const apiRequest = async (url, method = "GET", body = null) => {
+    const options = {
+        method,
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+    if (body) {
+        options.body = JSON.stringify(body);
+    }
+
+    const response = await fetch("${API_BASE_URL}${url}", options);
+    if (!response.ok){
+        throw new Error("Error: ${response.status}");
+    }
+    return response.json();
+};
