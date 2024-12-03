52ec81f05803922e0d2e1fadb43a48dd019d2c92
merlyngilchrist <157403080+merlyngilchrist@users.noreply.github.com>
2024-12-03 13:26:16 -0700

    Update gameLogic.js

--- ScattergoriesTogetherGUI/root/gameLogic.js
+++ ScattergoriesTogetherGUI/root/gameLogic.js
@@ -246,7 +246,7 @@ document.addEventListener("DOMContentLoaded", async () => {
                     }
 
                     console.log("Responses submitted:", responses);
-                    window.location.href = "results.html";
+                    window.location.href = "review.html";
                 });
             } else {
                 alert("Failed to load game data. Please try again");
